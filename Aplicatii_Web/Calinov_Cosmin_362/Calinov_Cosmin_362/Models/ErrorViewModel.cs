36fe9d1e6fa7ed63ea88644738b17756c10f1a7c
someonefromfmi <cosminov.calin@gmail.com>
2026-03-02 12:49:50 +0200

    Added Aplicatii_Web

---
+++ Aplicatii_Web/Calinov_Cosmin_362/Calinov_Cosmin_362/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Calinov_Cosmin_362.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
